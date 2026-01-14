ae0f294fb59053a570fad6d761ab43ccf93399b2
drzkn <diegorazq@gmail.com>
2026-01-14 12:03:19 +0100

    Phase 3.2 --> create adapter and normalizer for eslint vue plugin

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,3 +1,4 @@
 export * from './base.js';
 export * from './pa11y.js';
 export * from './axe.js';
+export * from './eslint.js';
--- src/normalizers/index.ts
+++ src/normalizers/index.ts
@@ -1,2 +1,3 @@
 export * from './base.js';
 export * from './pa11y.js';
+export * from './eslint.js';
