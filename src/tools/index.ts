104a57b1b31d0066b0deada9fc013fc2ab70cc4c
drzkn <diegorazq@gmail.com>
2026-02-10 15:36:04 +0100

    re export tool

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,3 +3,4 @@ export { analyzeWithAxeTool, disposeAxeAdapter } from './Axe/index.js';
 export { analyzeWithPa11yTool, disposePa11yAdapter } from './Pa11y/index.js';
 export { analyzeMixedTool, disposeAnalyzeMixedAdapters } from './AnalyzeMixed/index.js';
 export { analyzeContrastTool, disposeContrastAdapter } from './Contrast/index.js';
+export { analyzeWithLighthouseTool, disposeLighthouseAdapter } from './Lighthouse/index.js';
